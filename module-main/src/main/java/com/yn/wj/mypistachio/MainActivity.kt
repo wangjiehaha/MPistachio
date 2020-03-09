64b2558639bb6a3397a9a6e77e05d0ea98ae7d16
wj <3173917933@qq.com>
2020-03-10 00:16:35 +0800

    [feature]启动页配置完成

--- module-main/src/main/java/com/yn/wj/mypistachio/MainActivity.kt
+++ module-main/src/main/java/com/yn/wj/mypistachio/MainActivity.kt
@@ -2,6 +2,7 @@ package com.yn.wj.mypistachio
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import com.alibaba.android.arouter.launcher.ARouter
 import com.yn.wj.commonmodule.RouterTable
 
@@ -9,7 +10,11 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val handler = Handler()
+        handler.postDelayed({ startMain() }, 3 * 1000)
+    }
+
+    private fun startMain() {
         ARouter.getInstance().build(RouterTable.ACTIVITY_SIGN_IN).navigation()
         finish()
     }
